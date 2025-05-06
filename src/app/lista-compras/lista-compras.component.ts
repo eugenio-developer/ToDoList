6c6091c82e1ea750a201bbcf33dfb6ee89938b58
Eugenio Viana <eugenioviana.developer@gmail.com>
2025-05-06 13:45:37 -0300

    Adicionar item implementado

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
+
 export class ListaComprasComponent {
+
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adicionarItem(){
+    let itemlista = new ItemLista();
+    itemlista.nome = this.item;
+    itemlista.id = this.lista.length + 1;
+
+    this.lista.push(itemlista);
+
+    this.item = '';
+
+    console.table(this.lista);
+  }
+
 
 }
