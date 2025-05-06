4a40a257bd6144f16d2e577bb933644420035007
Eugenio Viana <eugenioviana.developer@gmail.com>
2025-05-06 12:26:28 -0300

    criando e estilizando o componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
